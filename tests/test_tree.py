cbaeda65b3ef64e6ea06a8201436c11ba425c5b6
Roman Arbuzov <lpandlipmbizkit@yandex.ru>
2025-01-17 19:36:34 +0300

    test: add some tests

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -20,7 +20,7 @@ class TestTreeStore(unittest.TestCase):
         self.assertListEqual(
             self.ts.get_all(),
             [
-                {"id": 1, "parent": "root", "type": None},
+                {"id": 1, "parent": "root"},
                 {"id": 2, "parent": 1, "type": "test"},
                 {"id": 3, "parent": 1, "type": "test"},
                 {"id": 4, "parent": 2, "type": "test"},
@@ -31,6 +31,14 @@ class TestTreeStore(unittest.TestCase):
             ],
         )
 
+    def test_get_existing_item(self):
+        self.assertEqual(self.ts.get_item(2), {"id": 2, "parent": 1, "type": "test"})
 
+    def test_get_non_existing_item(self):
+        self.assertIsNone(self.ts.get_item(-1))
+
+    def test_get_all_parent(self):
+        expected = [{"id":4,"parent":2,"type":"test"},{"id":2,"parent":1,"type":"test"},{"id":1,"parent":"root"}]
+        self.assertListEqual(self.ts.get_all_parents(7), expected)
 if __name__ == "__main__":
     unittest.main()
